e501a30b1c4f031c355e5fc65bb6cccee4ee5637
João Paulo <joaopaulo0907@gmail.com>
2023-02-26 11:42:18 -0300

    implementação da criptografia da senha do usuário antes de cadastrar no banco

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,12 +1,14 @@
 import {db} from '../database/postgres.js'
 import dayjs from 'dayjs';
-
+import bcrypt from 'bcryptjs';
 
 export async function registerUser(req, res){
     const {name, email, password } = req.body;
     const createdAt = dayjs();
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
     try {
-        await db.query(`INSERT INTO "public.users" (name, email, password, "createdAt") VALUES ($1, $2, $3, $4)`, [name, email, password, createdAt]);
+        await db.query(`INSERT INTO "public.users" (name, email, password, "createdAt") VALUES ($1, $2, $3, $4)`, [name, email, hashedPassword, createdAt]);
         return res.sendStatus(201);
     } catch (error) {
         return res.send(error.message);
