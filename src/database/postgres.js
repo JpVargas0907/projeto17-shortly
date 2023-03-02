658e499fe55eb5c0ee9fa1e7f5b83460351182fb
João Paulo Vargas <joao.vargas@seatecnologia.com.br>
2023-03-02 18:50:20 -0300

    update na config da conexão com o banco de dados

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -6,11 +6,8 @@ const { Pool } = pg;
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-  ...(process.env.MODE === "production" && {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }),
 };
+
+if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
