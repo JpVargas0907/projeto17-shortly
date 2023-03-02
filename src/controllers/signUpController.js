cd8aef481802e40f6236400fc58ef2f988d12575
João Paulo Vargas <joao.vargas@seatecnologia.com.br>
2023-03-02 17:41:35 -0300

    chore: adjust code for deploy on render

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,16 +1,23 @@
-import {db} from '../database/postgres.js'
-import dayjs from 'dayjs';
-import bcrypt from 'bcryptjs';
+import { db } from "../database/postgres.js";
+import dayjs from "dayjs";
+import bcrypt from "bcryptjs";
 
-export async function registerUser(req, res){
-    const {name, email, password } = req.body;
-    const createdAt = dayjs();
-    const hashedPassword = bcrypt.hashSync(password, 10);
+export async function registerUser(req, res) {
+  const { name, email, password } = req.body;
+  const createdAt = dayjs();
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-    try {
-        await db.query(`INSERT INTO "public.users" (name, email, password, "createdAt") VALUES ($1, $2, $3, $4)`, [name, email, hashedPassword, createdAt]);
-        return res.sendStatus(201);
-    } catch (error) {
-        return res.send(error.message);
-    }
+  try {
+    await db.query(
+      `
+      INSERT INTO 
+          "public.users" (name, email, password, "createdAt") 
+      VALUES 
+          ($1, $2, $3, $4)`,
+      [name, email, hashedPassword, createdAt]
+    );
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.send(error.message);
+  }
 }
